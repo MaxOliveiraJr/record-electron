96c77abcef7c342bc062120d3c0c9ac27bd18ab3
Max Oliveira <max-oliveira@outlook.com.br>
2023-02-21 16:31:57 -0300

    feat: listando lista de audio input

--- src/mainWindow/script.js
+++ src/mainWindow/script.js
@@ -1,8 +1,44 @@
 document.addEventListener('DOMContentLoaded',()=>{
-   setTimeout(()=>{
-       document.querySelector('#record').classList.add('recording')
-       document.querySelector('#mic-icon').classList.add('hide')
-   },2000)
+
+    const display = document.querySelector('#display');
+    const record = document.querySelector('#record');
+    const micInput = document.querySelector('#mic');
+
+    let isRecording = false;
+    let selectedDeviced = null;
+
+    // Get available devices
+
+    navigator.mediaDevices.enumerateDevices().then(devices=>{
+        devices.forEach(device=>{
+            if (device.kind === "audioinput"){
+                if(!selectedDeviced){
+                    selectedDeviced = device.deviceId;
+                }
+
+                const option = document.createElement('option');
+                option.value = device.deviceId;
+                option.text = device.label;
+                micInput.appendChild(option);
+            }
+        });
+    })
+
+    micInput.addEventListener('change',(event)=>{
+        selectedDeviced = event.target.value;
+        console.log(selectedDeviced);
+    })
+
+   function updateButtonTo(recording){
+    if(recording){
+        document.querySelector('#record').classList.add('recording');
+        document.querySelector('#mic-icon').classList.add('hide');
+    }else{
+        document.querySelector('#record').classList.remove('recording');
+        document.querySelector('#mic-icon').classList.remove('hide');
+    }
+   }    
+
 })
 
 window.onload = ()=>{
