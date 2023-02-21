0de35f690484c21d8d8bed1ff34937bc812289d9
Max Oliveira <max-oliveira@outlook.com.br>
2023-02-21 16:45:09 -0300

    feat: criando blob para armazenamento de chunks de audio

--- src/mainWindow/script.js
+++ src/mainWindow/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
 
     const display = document.querySelector('#display');
     const record = document.querySelector('#record');
@@ -6,13 +6,15 @@ document.addEventListener('DOMContentLoaded',()=>{
 
     let isRecording = false;
     let selectedDeviced = null;
+    let mediaRecorder = null;
+    let chucks = [];
 
     // Get available devices
 
-    navigator.mediaDevices.enumerateDevices().then(devices=>{
-        devices.forEach(device=>{
-            if (device.kind === "audioinput"){
-                if(!selectedDeviced){
+    navigator.mediaDevices.enumerateDevices().then(devices => {
+        devices.forEach(device => {
+            if (device.kind === "audioinput") {
+                if (!selectedDeviced) {
                     selectedDeviced = device.deviceId;
                 }
 
@@ -24,23 +26,61 @@ document.addEventListener('DOMContentLoaded',()=>{
         });
     })
 
-    micInput.addEventListener('change',(event)=>{
+    micInput.addEventListener('change', (event) => {
         selectedDeviced = event.target.value;
         console.log(selectedDeviced);
     })
 
-   function updateButtonTo(recording){
-    if(recording){
-        document.querySelector('#record').classList.add('recording');
-        document.querySelector('#mic-icon').classList.add('hide');
-    }else{
-        document.querySelector('#record').classList.remove('recording');
-        document.querySelector('#mic-icon').classList.remove('hide');
+    function updateButtonTo(recording) {
+        if (recording) {
+            document.querySelector('#record').classList.add('recording');
+            document.querySelector('#mic-icon').classList.add('hide');
+        } else {
+            document.querySelector('#record').classList.remove('recording');
+            document.querySelector('#mic-icon').classList.remove('hide');
+        }
     }
-   }    
+
+
+    record.addEventListener('click', () => {
+        updateButtonTo(!isRecording)
+
+        handleRecord(isRecording)
+
+        isRecording = !isRecording;
+    })
+
+    function handleRecord(recording) {
+        if (recording) {
+            mediaRecorder.stop();
+        } else {
+            navigator.mediaDevices.getUserMedia({
+                audio: {
+                    deviceId: selectedDeviced
+                }, video: false
+            }).then(stream => {
+                mediaRecorder = new MediaRecorder(stream)
+                mediaRecorder.start();
+                mediaRecorder.ondataavailable = (event) => {
+                    chucks.push(event.data);
+                }
+                mediaRecorder.onstop = (event) => {
+                    saveData();
+                }
+            })
+        }
+    }
+
+    function saveData() {
+        const blob = new Blob(chucks, { "type": "audio/webm;codecs=oputs" });
+
+        console.log(blob);
+        chucks = [];
+    }
+
 
 })
 
-window.onload = ()=>{
+window.onload = () => {
     document.body.classList.remove('preload');
 }
